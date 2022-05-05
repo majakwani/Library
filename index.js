0b37d6255e406038378f3656d8e83f57775b3b85
majakwani <anasjakwani72@gmail.com>
2022-05-05 10:31:03 +0500

    Made some changes in javascript file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const bookRead = document.querySelector('#read');
+const bookNotRead = document.querySelector('#not-read');
+const addBookButton = document.querySelector('.add-book');
+const editCard = document.querySelector('.edit-card')
+
 function Book(){
     this.title = title;
     this.author = author;
@@ -12,5 +20,16 @@ Book.prototype.printBook = function(){
 let myLibrary = [];
 
 function addBookToLibrary(){
+    let completed = false;
+    if (bookTitle.value.length == 0 || bookAuthor.value.length == 0 || bookPages.value.length == 0 || bookRead.checked == false && bookNotRead.checked == false){
+        alert("Input fields are empty");
+        return false;
+    }
 
+    if (bookRead.checked == true && bookNotRead.checked == false){
+        completed = true;
+    }
+    console.log(bookTitle.value)
+    let bookObject = {Title : bookTitle.value, Author : bookAuthor.value, Pages : bookPages.value, Read : completed};
+    myLibrary.push(bookObject);
 }
