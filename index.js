caa5b5ae10c5826ea7fb5e266d198684d628ccf1
majakwani <anasjakwani72@gmail.com>
2022-05-20 12:40:32 +0500

    slight changes

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ const editCard = document.querySelector('.edit-card');
 const rightSide = document.querySelector('.right-side');
 const form = document.querySelector('.form');
 const deleteBtn = document.querySelectorAll('.deleteButton');
+const completedBtn = document.querySelector('.completedButton');
 
 let myLibrary = [];
-let explored = [];
+let booksToDisplay = [];
 
+// Constructor Function
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
-
+// Adding a function to our constructor through prototype object
 Book.prototype.printBook = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
+// This function takes user input from a form, makes an object and add that object to an array.
 function addBookToLibrary(){
     let completed = false;
     if (bookTitle.value.length == 0 || bookAuthor.value.length == 0 || bookPages.value.length == 0 || bookRead.checked == false && bookNotRead.checked == false){
@@ -33,39 +36,63 @@ function addBookToLibrary(){
     if (bookRead.checked == true && bookNotRead.checked == false){
         completed = true;
         const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'Completed.')
-        myLibrary.push(book);
-        displayBook(myLibrary);
+        booksToDisplay.push(book);
+        displayBook(booksToDisplay);
         form.reset();
     }
     else{
         const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, 'Not Completed.')
-        myLibrary.push(book);
-        displayBook(myLibrary);
+        booksToDisplay.push(book);
+        displayBook(booksToDisplay);
         form.reset();
     }
 }
 
+// This function takes an array of books, display them on our HTML page.
 function displayBook(bookLibrary){
 
     for(let i = 0; i < bookLibrary.length; i++){
-        explored.push(bookLibrary[i]);
-        let number = explored.length;
+        siteContent = "";
+        myLibrary.push(bookLibrary[i]);
+        let number = myLibrary.length;
+
         const element = document.createElement('div');
         element.setAttribute('class' , 'card');
         element.classList.add(`book${number}`);
 
-    let siteContent = `<p> Book Number: ${number}
-    <p>Book Title: ${bookLibrary[i].title}</p>
-    <p>Book Author: ${bookLibrary[i].author}</p>
-    <p>Number of Pages: ${bookLibrary[i].pages}</p>
-    <p>Book Completed : ${bookLibrary[i].read}</p>
-    <p><button class="edit-card" type="button">Edit</button> <button type="button">Completed</button> <button type = "button" class = "deleteButton"> Delete </button></p>`;
-    element.innerHTML = siteContent;
-    rightSide.appendChild(element);
-    myLibrary.shift();
+        if (bookLibrary[i].read == 'Completed.'){
+        siteContent = `<p> Book Number: ${number}
+        <p>Book Title: ${bookLibrary[i].title}</p>
+        <p>Book Author: ${bookLibrary[i].author}</p>
+        <p>Number of Pages: ${bookLibrary[i].pages}</p>
+        <p>Book Completed : ${bookLibrary[i].read}</p>
+        <p><button class="edit-card" type="button">Edit</button> <button type="button" class = "completedButton">Completed</button> <button type = "button" class = "deleteButton"> Delete </button></p>`;
+        }
+
+        else if (bookLibrary[i].read == 'Not Completed.'){
+        siteContent = `<p> Book Number: ${number}
+        <p>Book Title: ${bookLibrary[i].title}</p>
+        <p>Book Author: ${bookLibrary[i].author}</p>
+        <p>Number of Pages: ${bookLibrary[i].pages}</p>
+        <p>Book Completed : ${bookLibrary[i].read}</p>
+        <p><button class="edit-card" type="button">Edit</button> <button type="button" class = "completedButton">Not Completed</button> <button type = "button" class = "deleteButton"> Delete </button></p>`;
+        }
+
+        element.innerHTML = siteContent;
+
+        rightSide.appendChild(element);
+        booksToDisplay.shift();
     }
 }
 
 deleteBtn.addEventListener('click', () => {
+
+})
+
+editCard.addEventListener('click', () => {
+
+})
+
+completedBtn.addEventListener('click' , () => {
 
 })
